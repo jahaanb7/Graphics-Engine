f21082256cd8ff2329d874ec45b56b0b45a58950
Jahaan <jeyubhavsar@gmail.com>
2026-01-17 17:03:58 -0500

    Refactor cube size and triangle definitions

--- src/MyMeshes.java
+++ src/MyMeshes.java
@@ -1,13 +1,9 @@
-
-public class MyMeshes{
+public class MyMeshes {
   public static Mesh cube;
 
   static {
-    double size = 10.0; //size of the length of the cube
+    double size = 1.0; // length
     cube = new Mesh();
-
-    System.out.println("Mesh created: " + cube);
-    System.out.println("Cube tris before add: " + cube.tris);
 
     Vector3D v0 = new Vector3D(-size, -size, -size);
     Vector3D v1 = new Vector3D(-size, -size,  size);
@@ -17,31 +13,29 @@ public class MyMeshes{
     Vector3D v5 = new Vector3D( size, -size,  size);
     Vector3D v6 = new Vector3D( size,  size, -size);
     Vector3D v7 = new Vector3D( size,  size,  size);
-    System.out.println("Debug: success");
 
+    // Front (+Z)
+    cube.tris.add(new Triangle(v1, v3, v5));
+    cube.tris.add(new Triangle(v3, v7, v5));
 
-    // Front face
-    cube.tris.add(new Triangle(v0, v1, v2));
-    cube.tris.add(new Triangle(v1, v3, v2));
+    // Back (-Z)
+    cube.tris.add(new Triangle(v4, v6, v0));
+    cube.tris.add(new Triangle(v6, v2, v0));
 
-    // Back face
-    cube.tris.add(new Triangle(v4, v6, v5));
-    cube.tris.add(new Triangle(v5, v6, v7));
+    // Left (-X)
+    cube.tris.add(new Triangle(v0, v2, v1));
+    cube.tris.add(new Triangle(v2, v3, v1));
 
-    // Left face
-    cube.tris.add(new Triangle(v0, v2, v4));
-    cube.tris.add(new Triangle(v4, v2, v6));
+    // Right (+X)
+    cube.tris.add(new Triangle(v5, v7, v4));
+    cube.tris.add(new Triangle(v7, v6, v4));
 
-    // Right face
-    cube.tris.add(new Triangle(v1, v5, v3));
-    cube.tris.add(new Triangle(v5, v7, v3));
+    // Top (+Y)
+    cube.tris.add(new Triangle(v2, v6, v3));
+    cube.tris.add(new Triangle(v6, v7, v3));
 
-    // Top face
-    cube.tris.add(new Triangle(v2, v3, v6));
-    cube.tris.add(new Triangle(v3, v7, v6));
-
-    // Bottom face
-    cube.tris.add(new Triangle(v0, v4, v1));
-    cube.tris.add(new Triangle(v1, v4, v5));
+    // Bottom (-Y)
+    cube.tris.add(new Triangle(v1, v5, v0));
+    cube.tris.add(new Triangle(v5, v4, v0));
   }
 }
